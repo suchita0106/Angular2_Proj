b1accdcc0a5a9ccc24d15d9b4e258765150cc70b
suchita0106 <suchita21.dabir@gmail.com>
2018-04-22 12:46:15 +0530

    Multiple components

--- first-application/src/app/app.component.ts
+++ first-application/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
    <h1>Heyy,create new component </h1>
    <app-other></app-other>
+   <app-another></app-another>
   `,
   styles: [`
   h1{
---
+++ first-application/src/app/other/another.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-another',
+  template: `
+    <p>
+      another Works!
+    </p>
+  `,
+  styles: []
+})
+export class AnotherComponent {
+
+}
