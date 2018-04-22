2dbdfab97cd13cd355dcac41cd80795fcb575242
suchita0106 <suchita21.dabir@gmail.com>
2018-04-22 12:35:39 +0530

    creating new component

--- first-application/src/app/app.component.ts
+++ first-application/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   <h1>Heyy,try another template style </h1>
-   <p>I hope u liked it! </p>
+   <h1>Heyy,create new component </h1>
+   <app-other></app-other>
   `,
-  styleUrls: ['./app.component.css']
+  styles: [`
+  h1{
+      color :red;
+  `]
 })
 export class AppComponent {
   title = 'app works, great!';
