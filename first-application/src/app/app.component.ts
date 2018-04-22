eff9e3ad84594a21f125ee7768dc205107a0a73c
suchita0106 <suchita21.dabir@gmail.com>
2018-04-22 11:44:27 +0530

    Template styles

--- first-application/src/app/app.component.ts
+++ first-application/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+   <h1>Heyy,try another template style </h1>
+   <p>I hope u liked it! </p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
